1c6eb74dd27782427070e0aa8dc700be7338b754
Vinicius Pires Barreto <viniciuspbarreto1@gmail.com>
2022-08-01 22:44:11 -0300

    ✨ feat: Callback Function
    Callback function to get token code from github

--- src/controllers/authGithubController.ts
+++ src/controllers/authGithubController.ts
@@ -1,10 +1,29 @@
+import axios from "axios";
 import { Request, Response } from "express";
-import { GITHUB_CLIENT_ID } from "../config";
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "../config";
 
 export class AuthGithubController {
-  public async handle(req: Request, res: Response) {
+  public async redirect(req: Request, res: Response) {
     res.redirect(
       `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}`
     );
+  }
+
+  public async callback(code: any, res: Response) {
+    const body = {
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+      code
+    };
+    const opts = { headers: { accept: "application/json" } };
+    axios
+      .post("https://github.com/login/oauth/access_token", body, opts)
+      .then(_res => _res.data.access_token)
+      .then(token => {
+        console.log("My token: ", token);
+
+        res.redirect(`/?token=${token}`);
+      })
+      .catch(err => res.status(500).json({ error: err.message }));
   }
 }
--- src/routes/authGithub.routes.ts
+++ src/routes/authGithub.routes.ts
@@ -4,6 +4,9 @@ import { AuthGithubController } from "../controllers/authGithubController";
 const authGithubRouter = Router();
 const authGithubController = new AuthGithubController();
 
-authGithubRouter.get("/", authGithubController.handle);
+authGithubRouter.get("/", authGithubController.redirect);
+authGithubRouter.get("/oauth-callback", ({ query: { code } }, res) => {
+  authGithubController.callback(code, res);
+});
 
 export { authGithubRouter };
