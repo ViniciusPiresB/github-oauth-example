2c34793ddfa6b09271f0fa49168b23c83c19e52f
Enzo <enzowederma@gmail.com>
2022-09-08 15:53:56 -0300

    ✨ feat: new profile
    Profile now contains public repositories and bio, and have been through a redesign.

--- src/html/js/renderGithubInfo.js
+++ src/html/js/renderGithubInfo.js
@@ -3,11 +3,13 @@ const userInfoDiv = document.getElementById("welcome-auth");
 const logoutScript = document.createElement("script");
 
 const userInfoContent =
-  '<img id="avatar" src="" alt="Avatar from github">\
-            <div id="user-information">\
-                <h1>Hello there, <span id="name"></span>! 👋</h1>\
-                <h2>🌎 <span id="location"></span></h2>\
-                <h2 >👤 Followers: <span id="followers"></span></h2>\
+  '<div id="user-information">\
+            <div id="avatar-container"><img id="avatar" src="" alt="Avatar from github"></div>\
+                <h1><span id="name"></span></h1>\
+                <h5><span id="bio"></span></h5>\
+                <h4 >📦 Public Repositories: <span id="repos"></span></h4>\
+                <h4 >👤 Followers: <span id="followers"></span></h4>\
+                <h4>🌎 <span id="location"></span></h4>\
                 <button id="logout-btn"><img src="imgs/logout-button.svg" alt="Logout Image"></button>\
             </div>';
 
@@ -26,13 +28,18 @@ if (userToken) {
   userInfoDiv.innerHTML = userInfoContent;
 
   const name = document.getElementById("name");
-  const location = document.getElementById("location");
   const avatar = document.getElementById("avatar");
+  const bio = document.getElementById("bio");
+  const repos = document.getElementById("repos");
   const followers = document.getElementById("followers");
+  const location = document.getElementById("location");
 
   name.innerText = info.name;
-  location.innerText = info.location;
+
   avatar.src = info.avatar_url;
+  bio.innerHTML = info.bio;
+  repos.innerText = info.public_repos;
+  location.innerText = info.location;
   followers.innerText = info.followers;
   logoutScript.setAttribute("src", "js/logout-user.js");
   document.getElementsByTagName("head")[0].appendChild(logoutScript);
